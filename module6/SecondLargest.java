ffc89a05380155bde14d6156e63b9ef71aeec33b
Namratha J <namrathan023@gmail.com>
2026-03-15 09:39:56 +0530

    Add files via upload

---
+++ module6/SecondLargest.java
@@ -0,0 +1,21 @@
+package module6;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 5, 8, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println("Second Largest: " + secondLargest);
+    }
+}
